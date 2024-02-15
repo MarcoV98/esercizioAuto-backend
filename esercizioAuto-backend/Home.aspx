475249f2db4c3df4c14d9aa8789632bfbc6779ba
MarcoV98 <148749261+MarcoV98@users.noreply.github.com>
2024-02-15 16:54:08 +0100

    Aggiungere i file di progetto.

---
+++ esercizioAuto-backend/Home.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Home.aspx.cs" Inherits="Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calcolatore Preventivo Auto</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Preventivo Auto</h1>
+
+            <asp:DropDownList ID="ddlAuto" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlAuto_SelectedIndexChanged">
+                <asp:ListItem Text="Fiat Panda" Value="FiatPanda" />
+                <asp:ListItem Text="Ford Fiesta" Value="FordFiesta" />
+                <asp:ListItem Text="Audi Q3" Value="AudiQ3" />
+                <asp:ListItem Text="Lamborghini Urus" Value="LamborghiniUrus" />
+            </asp:DropDownList>
+
+            <br />
+
+            <asp:Image ID="imgAuto" runat="server" Width="200px" Height="150px" />
+
+            <br />
+
+            <h3>Optional:</h3>
+            <asp:CheckBox ID="chkFinestrini" runat="server" Text="Finestrini automatici (EUR 100)" />
+            <br />
+            <asp:CheckBox ID="chkCerchi" runat="server" Text="Cerchi in lega (EUR 250)" />
+            <br />
+            <asp:CheckBox ID="chkStereo" runat="server" Text="Stereo integrato (EUR 500)" />
+
+            <br />
+            <br />
+
+            <h3>Garanzia (EUR 120/anno):</h3>
+            <asp:TextBox ID="txtGaranzia" runat="server"></asp:TextBox> anni
+
+            <br />
+            <br />
+
+            <asp:Button ID="btnCalcola" runat="server" Text="Calcola Preventivo" OnClick="btnCalcola_Click" />
+
+            <br />
+            <br />
+
+            <h3>Risultati:</h3>
+            <p>Prezzo di base: <asp:Label ID="lblPrezzoBase" runat="server" Text=""></asp:Label></p>
+            <p>Totale Optional: <asp:Label ID="lblTotaleOptional" runat="server" Text=""></asp:Label></p>
+            <p>Totale Garanzia: <asp:Label ID="lblTotaleGaranzia" runat="server" Text=""></asp:Label></p>
+            <p>Totale Complessivo: <asp:Label ID="lblTotaleComplessivo" runat="server" Text=""></asp:Label></p>
+
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ esercizioAuto-backend/Home.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace esercizioAuto_backend
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CaricaImmaginiAuto();
+            }
+        }
+
+        protected void ddlAuto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CaricaImmaginiAuto();
+        }
+
+        private void CaricaImmaginiAuto()
+        {
+            string autoSelezionata = ddlAuto.SelectedValue;
+            string percorsoImmagine = $"~/assets/{autoSelezionata.ToLower()}.jpg";
+
+            imgAuto.ImageUrl = percorsoImmagine;
+
+            switch (autoSelezionata)
+            {
+                case "FiatPanda":
+                    lblPrezzoBase.Text = "Prezzo di base: EUR 10,000";
+                    break;
+                case "FordFiesta":
+                    lblPrezzoBase.Text = "Prezzo di base: EUR 20,000";
+                    break;
+                case "AudiQ3":
+                    lblPrezzoBase.Text = "Prezzo di base: EUR 40,000";
+                    break;
+                case "LamborghiniUrus":
+                    lblPrezzoBase.Text = "Prezzo di base: EUR 200,000";
+                    break;
+            }
+        }
+
+        protected void btnCalcola_Click(object sender, EventArgs e)
+        {
+            CalcolaPreventivo();
+        }
+
+        private void CalcolaPreventivo()
+        {
+            string autoSelezionata = ddlAuto.SelectedValue;
+            decimal prezzoBase = 0;
+
+            switch (autoSelezionata)
+            {
+                case "FiatPanda":
+                    prezzoBase = 10000;
+                    break;
+                case "FordFiesta":
+                    prezzoBase = 20000;
+                    break;
+                case "AudiQ3":
+                    prezzoBase = 40000;
+                    break;
+                case "LamborghiniUrus":
+                    prezzoBase = 200000;
+                    break;
+            }
+
+            decimal totaleOptional = 0;
+            if (chkFinestrini.Checked)
+            {
+                totaleOptional += 100;
+            }
+            if (chkCerchi.Checked)
+            {
+                totaleOptional += 250;
+            }
+            if (chkStereo.Checked)
+            {
+                totaleOptional += 500;
+            }
+
+            
+            int anniGaranzia;
+            if (int.TryParse(txtGaranzia.Text, out anniGaranzia))
+            {
+                decimal totaleGaranzia = anniGaranzia * 120;
+
+                
+                decimal totaleComplessivo = prezzoBase + totaleOptional + totaleGaranzia;
+
+                
+                lblPrezzoBase.Text = "Prezzo di base: " + prezzoBase.ToString("C");
+                lblTotaleOptional.Text = "Totale Optional: " + totaleOptional.ToString("C");
+                lblTotaleGaranzia.Text = "Totale Garanzia: " + totaleGaranzia.ToString("C");
+                lblTotaleComplessivo.Text = "Totale Complessivo: " + totaleComplessivo.ToString("C");
+            }
+            else
+            {
+                lblTotaleGaranzia.Text = "Errore: Inserisci un numero valido per la garanzia.";
+            }
+        }
+    }
+}
